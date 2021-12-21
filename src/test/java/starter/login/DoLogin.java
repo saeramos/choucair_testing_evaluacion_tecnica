b49062613d55717c75bf8463093841198bea033d
Sergio Escobar <saer014@gmail.com>
2021-12-21 17:21:59 -0500

    Class for test LoginForm

--- src/test/java/starter/login/DoLogin.java
+++ src/test/java/starter/login/DoLogin.java
@@ -1,4 +1,32 @@
 package starter.login;
 
-public class DoLogin {
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Performable;
+import net.serenitybdd.screenplay.Task;
+import net.serenitybdd.screenplay.actions.Click;
+import net.serenitybdd.screenplay.actions.Enter;
+
+import static net.serenitybdd.screenplay.Tasks.instrumented;
+
+public class DoLogin implements Task {
+    private final String username;
+    private final String password;
+
+    public DoLogin(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public static Performable Credentials(String username, String password){
+        return instrumented(DoLogin.class, username, password);
+    }
+    @Override
+    public <T extends Actor> void performAs(T actor) {
+        actor.attemptsTo(
+                Enter.theValue(username).into(LoginForm.LOGINEMAIL),
+                Enter.theValue(password).into(LoginForm.LOGINPASWORD),
+                Click.on(LoginForm.BTNLOGIN)
+        );
+    }
+
 }
